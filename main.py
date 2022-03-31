12f9194b6650690c1eb0982008a1748b49a82aa6
samuelcregan17 <samuelcregan17@augustana.edu>
2022-03-31 11:42:33 -0500

    Set up video feed on main

--- main.py
+++ main.py
@@ -1,17 +1,36 @@
-# This is a sample Python script.
+#Main class for project, this is where the camera feed loop will exist
+import cv2
+import numpy as np
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+#main method is where camera loop exists
+def main():
 
+    #setup video feed
+    cap = cv2.VideoCapture(0)
+    cap.set(3, 640) #set width
+    cap.set(4, 480) #set height
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    #while loop for camera feed
+    while True:
+        #preset color values for detecting white
+        h_min = 0
+        h_max = 179
+        l_min = 233
+        l_max = 255
+        s_min = 0
+        s_max = 255
 
+        #create mask and display original feed and the mask
+        _, img = cap.read()
+        imgHLS = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
+        cv2.imshow("Original", img)
+        lower = np.array([h_min, l_min, s_min])
+        upper = np.array([h_max, l_max, s_max])
+        mask = cv2.inRange(imgHLS, lower, upper)
+        imgResults = cv2.bitwise_and(img, img, mask=mask)
+        cv2.imshow("Results", imgResults) #may delete this from final code, don't think we need the mask displayed, good for debugging
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+        if cv2.waitKey(1) & 0xFF ==ord('q'):
+            cv2.destroyAllWindows()
 
-# Hi from Justin
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+main()
